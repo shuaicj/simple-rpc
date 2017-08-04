d3a2af86fc3711006464ec1e21630e8d83ebf15c
shuaicj <shuaicj@gmail.com>
2017-08-04 18:47:57 +0800

    add rpc-common: define a HelloService interface

---
+++ rpc-common/src/main/java/shuaicj/example/simplerpc/common/HelloService.java
@@ -0,0 +1,11 @@
+package shuaicj.example.simplerpc.common;
+
+/**
+ * A common interface for greeting.
+ *
+ * @author shuaicj 2017/08/04
+ */
+public interface HelloService {
+
+    String greet(String name, String country);
+}
